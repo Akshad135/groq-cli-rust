6a1f306d0f59460897a194be8950fc94578a35e6
DARKLORD513 <akshad.agrawal1978@gmail.com>
2024-06-09 01:14:08 +0530

    made it advanced :D

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{self, Write};
-use std::fs::{read, File};
+use std::io::{self, BufWriter, Write};
+use std::fs::{read, File, OpenOptions};
 use std::path::{self, Path};
 use serde::{Deserialize, Serialize};
 use std::io::BufReader;
@@ -9,10 +9,50 @@ struct ApiInfo{
     api_key : String,
     model : String,
 }
-
+    
 fn main()-> Result<(), Box<dyn std::error::Error>>{
+        
+        
+    let path_to_file = std::path::Path::new("config.json");
+    match File::open(path_to_file){
+        Ok(file) => file,
+        Err(ref e) if e.kind() == io::ErrorKind::NotFound  => {File::create(path_to_file)?},
+        Err(e) => {println!("Error: {}", e); return Err(Box::new(e));}
+    };
 
-// ? Getting the api key
+    // ? reading the json file
+    let extracted_info: ApiInfo = match read_json(path_to_file){
+        Ok(values) => values,
+        Err(ref e) if e.kind() == io::ErrorKind::UnexpectedEof => {info()},
+        Err(e) => return Err(Box::new(e))
+    };
+
+    let _ = write_json(path_to_file, &extracted_info);
+    println!("\nApi: {}\nModel: {}", extracted_info.api_key, extracted_info.model);
+
+    Ok(())
+}
+
+fn read_json(file_path : &Path) -> Result<ApiInfo, io::Error>{
+
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    let extractor = serde_json::from_reader(reader)?;
+    Ok(extractor)
+}
+
+fn write_json(file_path : &Path, data: &ApiInfo) -> Result<(), io::Error>{
+    let file = OpenOptions::new().write(true).truncate(true).open(file_path)?;
+
+    let mut writer = BufWriter::new(file);
+    let _ = serde_json::to_writer(&mut writer, &data)?;
+    Ok(())
+}
+
+
+fn info() -> ApiInfo {
+    // ? Getting the api key
     let mut api_key = String::new();
     print!("Enter your Api Key: ");
     io::stdout().flush().expect("Failed to flush stdout");
@@ -35,21 +75,20 @@ fn main()-> Result<(), Box<dyn std::error::Error>>{
         print!("  Select your model: " );
         io::stdout().flush().expect("Failed to flush the print buffer");
 
-    
+        model_choice.clear();
         io::stdin().read_line(&mut model_choice)
             .expect("Can't read the model input");
 
-        let model_choice = match model_choice.trim().parse::<i32>(){
+        match model_choice.trim().parse::<i32>(){
             Ok(1) => {model_choice = "LLaMA3 8b".to_string(); break},
             Ok(2) => {model_choice = "Mixtral 8x7b".to_string(); break},
             Ok(3) => {model_choice = "LLaMA3 70b".to_string(); break},
             Ok(4) => {model_choice = "Gemma 7b".to_string(); break},
             Ok(_) => {println!("    Enter an option between 1-4"); continue},
-            Err(_) => {println!("Invalid int"); continue},
+            Err(_) => {println!("   Enter a valid option"); continue},
         };
         
     }
-
 
     // ? Adding the final choice to the struct
     let final_choice = ApiInfo{
@@ -57,25 +96,5 @@ fn main()-> Result<(), Box<dyn std::error::Error>>{
         model : model_choice
     };
 
-    println!("\nApi key: {}\nModel: {}", final_choice.api_key, final_choice.model);
-
-    // ? reading the json file
-    let path_to_file = std::path::Path::new("test.json");
-    let extracted_info: ApiInfo = read_json(path_to_file)?;
-    println!("\nApi: {}\nModel: {}", extracted_info.api_key, extracted_info.model);
-
-    Ok(())
-}
-
-
-
-fn read_json(file_path : &Path) -> Result<ApiInfo, io::Error>{
-
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
-
-    let extractor = serde_json::from_reader(reader)?;
-    Ok(extractor)
-
-
+    return final_choice
 }
